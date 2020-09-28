77e3c5cadcdf93108b516406a61714a2fe6cf479
Daiana-Brites-Kirschner <daia.brites@gmail.com>
2020-09-28 19:41:58 -0300

    Criado teste de API utilizando JAVA e reportando através do relatório do Allure, foi feito teste de contrato e aceitação

---
+++ src/test/java/br/com/restassuredapitesting/utils/Utils.java
@@ -0,0 +1,32 @@
+package br.com.restassuredapitesting.utils;
+
+import org.json.simple.JSONObject;
+
+public class Utils {
+
+    public static JSONObject validaPayloadBooking() {
+        JSONObject payload = new JSONObject();
+
+        JSONObject bookingDates = new JSONObject();
+        bookingDates.put("checkin", "2018-01-01");
+        bookingDates.put("checkout","2019-01-01");
+
+        payload.put("firstname","Ronaldo");
+        payload.put("lastname","fenomeno");
+        payload.put("totalprice",111);
+        payload.put("depositpaid",true);
+        payload.put("bookingdates", bookingDates);
+        payload.put("additionalneeds","Breakfast");
+
+        return payload;
+    }
+
+    public static  String getContractsBasePath(String pack, String contract) {
+        return System.getProperty("user.dir")
+                + "/src/test/java/br/com/restassuredapitesting/tests/"
+                + pack
+                + "/contracts/"
+                + contract
+                + ".json";
+    }
+}
