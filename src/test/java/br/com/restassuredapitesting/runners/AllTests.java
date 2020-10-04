b454f3397585a35fc2cce5c1abb1e5f6d3318932
Daiana-Brites-Kirschner <daia.brites@gmail.com>
2020-10-04 01:03:23 -0300

    configurada a opcao para executar todos os 25 testes de uma vez via Runner AllTests

--- src/test/java/br/com/restassuredapitesting/runners/AllTests.java
+++ src/test/java/br/com/restassuredapitesting/runners/AllTests.java
@@ -1,14 +1,28 @@
 package br.com.restassuredapitesting.runners;
 
-import br.com.restassuredapitesting.tests.base.tests.BaseTest;
+import br.com.restassuredapitesting.tests.booking.tests.DeleteBookingTest;
+import br.com.restassuredapitesting.tests.booking.tests.GetBookingTest;
+import br.com.restassuredapitesting.tests.booking.tests.PostBookingTest;
+import br.com.restassuredapitesting.tests.booking.tests.PutBookingTest;
+import br.com.restassuredapitesting.tests.healthcheck.tests.GetHealthcheckTest;
 import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Categories.class)
-@Categories.IncludeCategory(br.com.restassuredapitesting.suites.AllTests.class)
-@Suite.SuiteClasses({
-        BaseTest.class,
+@Categories.IncludeCategory({
+        br.com.restassuredapitesting.suites.Acceptance.class,
+        br.com.restassuredapitesting.suites.Contract.class,
+        br.com.restassuredapitesting.suites.E2e.class,
+        br.com.restassuredapitesting.suites.Healthcheck.class
 })
+@Suite.SuiteClasses({
+        DeleteBookingTest.class,
+        GetBookingTest.class,
+        PostBookingTest.class,
+        PutBookingTest.class,
+        GetHealthcheckTest.class
+})
+
 public class AllTests {
 }
